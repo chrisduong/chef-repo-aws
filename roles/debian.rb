3fc0e7c88af4f3acba5bbcc686f9a649e098c154
Chris Duong <chris.duong83@gmail.com>
2014-05-03 15:39:04 +0700

    added new debian role and update developer's role

---
+++ roles/debian.rb
@@ -0,0 +1,9 @@
+name "debian"
+description "Attributes specific to the Debian platform family"
+run_list(
+)
+default_attributes(
+  "vagrant" => {
+    "url" => "https://dl.bintray.com/mitchellh/vagrant/#vagrant_1.4.3_x86_64.deb"
+  }
+)
