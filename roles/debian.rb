2b4ee1e9b1d42e71b2bccafe1456a2cfb6800906
Chris Duong <chris.duong83@gmail.com>
2014-05-06 11:06:08 +0700

    update roles

--- roles/debian.rb
+++ roles/debian.rb
@@ -4,6 +4,6 @@ run_list(
 )
 default_attributes(
   "vagrant" => {
-    "url" => "https://dl.bintray.com/mitchellh/vagrant/#vagrant_1.4.3_x86_64.deb"
+    "url" => "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.3_x86_64.deb"
   }
 )
