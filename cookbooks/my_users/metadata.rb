7f7287102c0e30117b7099b9563a39bc89365a0d
Chris Duong <chris.duong83@gmail.com>
2014-08-25 11:17:07 +0700

    not using "a" in version

--- cookbooks/my_users/metadata.rb
+++ cookbooks/my_users/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chris.duong83@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures my_users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1a'
+version          '0.1.1'
 
 depends 'users'
 depends 'sudo'
