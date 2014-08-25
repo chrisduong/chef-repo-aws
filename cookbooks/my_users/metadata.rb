54cf531e94de0852099c3cf2e64bc0709ca57120
Chris Duong <chris.duong83@gmail.com>
2014-08-25 11:13:34 +0700

    test chef users

---
+++ cookbooks/my_users/metadata.rb
@@ -0,0 +1,10 @@
+name             'my_users'
+maintainer       'chrisD'
+maintainer_email 'chris.duong83@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures my_users'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1a'
+
+depends 'users'
+depends 'sudo'
