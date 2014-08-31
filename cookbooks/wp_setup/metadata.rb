6227bb50462f1c156435b7b1228ffeb8aaf4ca0e
Chris Duong <chris.duong83@gmail.com>
2014-08-31 21:30:15 +0700

    fix attribute name

---
+++ cookbooks/wp_setup/metadata.rb
@@ -0,0 +1,7 @@
+name             'wp_setup'
+maintainer       'chrisD'
+maintainer_email 'chris.duong83@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures wp_setup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/wp_setup/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: wp_setup
+# Recipe:: default
+#
+# Copyright 2014, chrisD
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+wp_setup "extracting wordpress" do
+  wp_path "node['wordpress']['path']"
+  wp_url  "node['wordpress']['url']"
+  action :extract
+end
+
+wp_setup "deleting the installation file" do
+  wp_path "node['wordpress']['path']"
+  action :remove
+end
